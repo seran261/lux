192d09cd9cca296fb9164da0ec83b2f2d956d053
seran261 <seran261@gmail.com>
2026-01-03 21:42:02 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,26 +1,24 @@
 import pandas as pd
-import numpy as np
 
 def ema(series, length):
     return series.ewm(span=length, adjust=False).mean()
 
-def generate_signal(df, ema_len):
+def generate_signal(df, ema_len, rr):
     df["ema"] = ema(df["close"], ema_len)
 
-    last = df.iloc[-1]
     prev = df.iloc[-2]
+    last = df.iloc[-1]
 
-    long_signal = prev["close"] < prev["ema"] and last["close"] > last["ema"]
-    short_signal = prev["close"] > prev["ema"] and last["close"] < last["ema"]
-
-    if long_signal:
+    # LONG
+    if prev["close"] < prev["ema"] and last["close"] > last["ema"]:
         sl = df["low"].rolling(10).min().iloc[-1]
-        tp = last["close"] + (last["close"] - sl) * 2
+        tp = last["close"] + (last["close"] - sl) * rr
         return "LONG", last["close"], sl, tp
 
-    if short_signal:
+    # SHORT
+    if prev["close"] > prev["ema"] and last["close"] < last["ema"]:
         sl = df["high"].rolling(10).max().iloc[-1]
-        tp = last["close"] - (sl - last["close"]) * 2
+        tp = last["close"] - (sl - last["close"]) * rr
         return "SHORT", last["close"], sl, tp
 
     return None
