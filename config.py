d03e23b4a03c148bb96070a1a68302641f36530e
seran261 <seran261@gmail.com>
2026-01-04 13:14:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os, sys
 
-# ===== TELEGRAM =====
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
@@ -8,11 +7,19 @@ if not TELEGRAM_TOKEN or not CHAT_ID:
     print("❌ TELEGRAM_TOKEN or CHAT_ID missing")
     sys.exit(1)
 
-# ===== STRATEGY =====
-TIMEFRAME = "5m"     # OKX format: 1m, 5m, 15m, 1H
 EMA_LENGTH = 34
-RR_RATIO = 2.0
+STOP_LOSS_PCT = 1.2
 
-# ===== BOT =====
+TP_LEVELS = [1, 2, 3, 4, 5]  # percent
+
+TIMEFRAMES = {
+    "5m": "5m",
+    "15m": "15m",
+    "1H": "1H",
+    "4H": "4H",
+    "1D": "1D",
+    "1W": "1W"
+}
+
 SCAN_INTERVAL = 60
 HEARTBEAT_INTERVAL = 900
