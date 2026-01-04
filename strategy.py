c11918db073e0dfcc7b3e2c0c06b7c34d7b51b0a
seran261 <seran261@gmail.com>
2026-01-04 13:15:22 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,22 +1,16 @@
 def ema(series, length):
     return series.ewm(span=length, adjust=False).mean()
 
-def generate_signal(df, ema_len, rr):
+def check_signal(df, ema_len):
     df["ema"] = ema(df["close"], ema_len)
 
     prev = df.iloc[-2]
     last = df.iloc[-1]
 
-    # LONG
     if prev["close"] < prev["ema"] and last["close"] > last["ema"]:
-        sl = df["low"].rolling(10).min().iloc[-1]
-        tp = last["close"] + (last["close"] - sl) * rr
-        return "LONG", last["close"], sl, tp
+        return "LONG"
 
-    # SHORT
     if prev["close"] > prev["ema"] and last["close"] < last["ema"]:
-        sl = df["high"].rolling(10).max().iloc[-1]
-        tp = last["close"] - (sl - last["close"]) * rr
-        return "SHORT", last["close"], sl, tp
+        return "SHORT"
 
     return None
